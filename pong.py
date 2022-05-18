db49f3642556a49a6d2363b9fccb42bcc23103e8
TinNguyen <ductin16111@gmail.com>
2022-05-19 02:03:29 +0300

    add setup codes for paddle A

--- pong.py
+++ pong.py
@@ -3,8 +3,21 @@ import turtle
 wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
-wn.setup(width = 800, height = 600)
+wn.setup(width = 800, height = 600) #0;0 is the center with edges (-400, 400) and (-300, 300)
 wn.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)   # not the speed of the paddle moving on the screen
+                    # it's the speed of animation. 0 = maximum speed
+paddle_a.shape('square')    # size is 20px x 20px
+paddle_a.color('white')
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)    # wid = 100px, len = 20px
+paddle_a.up()
+paddle_a.goto(-350, 0)
+# Paddle B
+
+# Ball
 
 # Main game loop
 while(True):
